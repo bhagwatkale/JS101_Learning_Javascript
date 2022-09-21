901c05270c0f0ecb12ef65f0216481ce2b97c7e1
Bhagwat Kale <bhagwatr5253@gmail.com>
2022-09-21 21:08:53 +0530

    1st pull

---
+++ problem2.js
@@ -0,0 +1,13 @@
+// Define a variable called name with your Name as the assigned value
+// Print the value stored in the variable name
+// Change the variable to store your father's name
+// Print the value stored in the variable name
+// Change the variable again to store your mother's name.
+// Print the value stored in the variable name
+
+let youName = "Bhagwat Kale";
+console.log(youName);
+let fathersName = "Ramchandra Kale";
+console.log(fathersName);
+let mothersName = "Rukhmin Kale";
+console.log(mothersName);
---
+++ problem4.js
@@ -0,0 +1,27 @@
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student.
+
+
+
+console.log("█▀▄▀█ ▄▀█ █▀█ █▄▀   █▀▄▀█ █▀▀ █▀▄▀█ █▀█");
+console.log("█░▀░█ █▀█ █▀▄ █░█   █░▀░█ ██▄ █░▀░█ █▄█");
+console.log("****************************************");
+
+let name = "Bhagwat Kale";
+let school = "Nutan Vidyalaya Sailu";
+let grade = "A+";
+let section = "B";
+let rollNo = 25;
+let marathi = 84;
+let hindi = 89;
+let english = 73;
+console.log("Name:", name);
+console.log("School Name:", school);
+console.log("Grade:", grade);
+console.log("Section:", section);
+console.log("Roll No.:", rollNo);
+console.log("Marathi:", marathi);
+console.log("Hindi:", hindi);
+console.log("English:", english);
+
+
